c3327cb74ea7b79080cd6406e6627db59cf826ec
Kory <kory.porter@hotmail.com>
2020-09-23 07:18:10 +1000

    started project 7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Kory Porter on 23/9/20.
+//  Copyright © 2020 Kory Porter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
