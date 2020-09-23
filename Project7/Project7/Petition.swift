ed21755f2bea608d7133369a80721921941ad3d5
Kory <kory.porter@hotmail.com>
2020-09-23 20:41:38 +1000

    finished day 33

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,13 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Kory Porter on 23/9/20.
+//  Copyright © 2020 Kory Porter. All rights reserved.
+//
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,11 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Kory Porter on 23/9/20.
+//  Copyright © 2020 Kory Porter. All rights reserved.
+//
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
